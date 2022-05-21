7e022a8fc7f72610fc12bae2c62f0fb87f3894c1
Francis Aguinaldo <fryaguinaldo@gmail.com>
2022-05-21 09:30:24 -0700

    add updates

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,7 +5,25 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      // be sure to include its associated Products
+      include: [
+        {
+          model: Product,
+          attributes: ['product_name']
+        },
+      ],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No categories were found." });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
